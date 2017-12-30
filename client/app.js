21e111b2ffdc5a45752c087262ef73f8e34f0c28
liangshaojie <877942720>
2017-12-30 17:06:56 +0800

    chushihua

---
+++ client/app.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './App.jsx'
+import { AppContainer } from "react-hot-loader"
+
+// ReactDOM.hydrate(<App />,document.getElementById('root'));
+const root = document.getElementById('root')
+const render = Component => {
+    ReactDOM.hydrate(
+        <AppContainer>
+            <Component/>
+        </AppContainer>,
+        root
+    )
+}
+
+render(App)
+
+if(module.hot){
+    module.hot.accept('./App.jsx',()=>{
+        const NextApp = requirt('./App.jsx').default
+        render(NextApp)
+        // ReactDOM.hydrate(<NextApp />,document.getElementById('root'));
+    })
+}
