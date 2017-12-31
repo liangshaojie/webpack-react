b4d2d2f5ec5bd638abcbeadb1a7e89cba28d9313
liangshaojie <877942720>
2017-12-31 17:59:05 +0800

    工程完毕

--- build/webpack.config.client.js
+++ build/webpack.config.client.js
@@ -1,69 +1,46 @@
 const path = require('path');
 const HTMLPlugin = require('html-webpack-plugin');
 const webpack = require('webpack');
+const webpackMerge = require("webpack-merge")
+const baseConfig = require("./webpack.base")
 const isDev = process.env.NODE_ENV === 'development'
 
 
-const config = {
-    entry: {
-        app:path.join(__dirname,'../client/app.js')
-    },
-    output:{
-        filename:'[name].[hash].js',
-        path:path.join(__dirname,'../dist'),
-        publicPath:'/public/'
-    },
-    module:{
-        rules:[
-            {
-                enforce:'pre',
-                test:/.(js|jsx)$/,
-                loader:"eslint-loader",
-                exclude:[
-                    path.resolve(__dirname,'../node_modules')
-                ]
-            },
-            {
-                test:/.jsx$/,
-                loader:'babel-loader'
-            },
-            {
-                test:/.js$/,
-                loader:'babel-loader',
-                exclude:[
-                    path.join(__dirname,'../node_modules')
-                ]
-            }
-        ]
-    },
-    plugins:[
-        new HTMLPlugin({
-            template:path.join(__dirname,'../client/template.html')
-        })
-    ]
-}
+const config = webpackMerge(baseConfig, {
+  entry: {
+    app: path.join(__dirname, '../client/app.js')
+  },
+  output: {
+    filename: '[name].[hash].js',
+  },
+  plugins: [
+    new HTMLPlugin({
+      template: path.join(__dirname, '../client/template.html')
+    })
+  ]
+})
 
-if(isDev){
-    config.entry = {
-        app:[
-            'react-hot-loader/patch',
-            path.join(__dirname,'../client/app.js')
-        ]
+if (isDev) {
+  config.entry = {
+    app: [
+      'react-hot-loader/patch',
+      path.join(__dirname, '../client/app.js')
+    ]
+  }
+  config.devServer = {
+    host: '127.0.0.1',
+    port: '1234',
+    contentBase: path.join(__dirname, '../dist'),
+    hot: true,
+    overlay: {
+      error: true,
+    },
+    publicPath: '/public/',
+    historyApiFallback: {
+      index: '/public/index.html'
     }
-    config.devServer = {
-        host:'127.0.0.1',
-        port:'1234',
-        contentBase:path.join(__dirname,'../dist'),
-        hot:true,
-        overlay:{
-            error:true,
-        },
-        publicPath:'/public/',
-        historyApiFallback:{
-            index:'/public/index.html'
-        }
-    }
-    config.plugins.push(new webpack.HotModuleReplacementPlugin())
+  }
+  config.plugins.push(new webpack.HotModuleReplacementPlugin())
 }
 
 
