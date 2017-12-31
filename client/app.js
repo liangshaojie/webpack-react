82fe36d60ad619669f9975cb2be8b9bf6acbba0d
liangshaojie <877942720>
2017-12-31 17:08:25 +0800

    tddd

--- client/app.js
+++ client/app.js
@@ -1,25 +1,25 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App.jsx'
-import { AppContainer } from "react-hot-loader"
+import {AppContainer} from "react-hot-loader" //eslint-disable-line
 
 // ReactDOM.hydrate(<App />,document.getElementById('root'));
 const root = document.getElementById('root')
-const render = Component => {
-    ReactDOM.hydrate(
-        <AppContainer>
-            <Component/>
-        </AppContainer>,
-        root
-    )
+const render = (Component) => {
+  ReactDOM.hydrate(
+    <AppContainer>
+      <Component />
+    </AppContainer>,
+    root,
+  )
 }
 
 render(App)
 
-if(module.hot){
-    module.hot.accept('./App.jsx',()=>{
-        const NextApp = requirt('./App.jsx').default
-        render(NextApp)
-        // ReactDOM.hydrate(<NextApp />,document.getElementById('root'));
-    })
+if (module.hot) {
+  module.hot.accept('./App.jsx', () => {
+    const NextApp = requirt('./App.jsx').default //eslint-disable-line
+    render(NextApp)
+    // ReactDOM.hydrate(<NextApp />,document.getElementById('root'));
+  })
 }
--- server/server.js
+++ server/server.js
@@ -3,24 +3,21 @@ const ReactSSR = require('react-dom/server');
 const fs = require('fs');
 const path = require('path');
 const isDev = process.env.NODE_ENV === 'development'
-
 const app = express();
 
-
-
-if(!isDev){
-    const serverEntry = require('../dist/server-entry').default;
-    const template = fs.readFileSync(path.join(__dirname,'../dist/index.html'),'utf8');
-    app.use('/public',express.static(path.join(__dirname,'../dist')));
-    app.get("*",function(req,res){
-        const appString = ReactSSR.renderToString(serverEntry)
-        res.send(template.replace('<!-- app -->',appString))
-    })
-}else{
-    const devStatic = require('./until/dev-statice.js')
-    devStatic(app)
+if (!isDev) {
+  const serverEntry = require('../dist/server-entry').default;
+  const template = fs.readFileSync(path.join(__dirname, '../dist/index.html'), 'utf8');
+  app.use('/public', express.static(path.join(__dirname, '../dist')));
+  app.get("*", function (req, res) {
+    const appString = ReactSSR.renderToString(serverEntry)
+    res.send(template.replace('<!-- app -->', appString))
+  })
+} else {
+  const devStatic = require('./until/dev-statice.js')
+  devStatic(app)
 }
 
-app.listen(2234,function () {
-    console.log("server is listening on 1236")
+app.listen(2234, function () {
+  console.log("server is listening on 2234")
 })
